aec35a49e4e2b59c7d0a7ad45300c165883889ff
Jesse Hallett <hallettj@gmail.com>
2009-05-27 18:11:40 -0700

    * Updated volcano distance calculation to handle calculations that cross   over the 180/-180 degree longitude line. * Removed artificial delay from volcano clustering callback.

--- volcano_clusters.js
+++ volcano_clusters.js
@@ -4,12 +4,12 @@
 (function($) {
 
  function coord_func(e) {
-   var x, y;
+   var x, y, meters_per_degree = 111132.09;
 
    x = e.lng;
    y = e.lat;
 
-   // Handle case where coordinates wrap over 0 degrees longitude.
+   // Handle case where coordinates wrap over 180/-180 degrees longitude.
    if (x < 0) {
      x += 360;
    }
@@ -17,7 +17,8 @@
    // Scale longitude degrees appropriately for the given latitude.
    x = x * Math.cos((y * 2.0 * Math.PI) / 360);
 
-   return [x, y];
+   // Return coordinates expressed as meters from 0, 0.
+   return [x * meters_per_degree, y * meters_per_degree];
  }
 
  $(document).ready(function() {
--- volcano_worker.js
+++ volcano_worker.js
@@ -10,25 +10,38 @@ function pausecomp(millis) {
   while(curDate-date < millis);
 } 
 
-// Returns the distance between two objects in meters. Both objects must have
+function coord_func(e) {
+  var x, y, meters_per_degree = 111132.09;
+
+  x = e.lng;
+  y = e.lat;
+
+  // Handle case where coordinates wrap over 180/-180 degrees longitude.
+  if (x < 0) {
+    x += 360;
+  }
+
+  // Scale longitude degrees appropriately for the given latitude.
+  x = x * Math.cos((y * 2.0 * Math.PI) / 360);
+
+  // Return coordinates expressed as meters from 0, 0.
+  return [x * meters_per_degree, y * meters_per_degree];
+}
+
+// Returns the distance between to objects in meters. Both objects must have
 // 'lat' and 'lng' properties.
 function geo_distance(a, b) {
-  var a_x, a_y, b_x, b_y, meters_per_degree = 111132.09;
-  a_x = meters_per_degree * a.lat;
-  a_y = meters_per_degree * a.lng * Math.cos((a.lat * 2.0 * Math.PI) / 360.0);
-  b_x = meters_per_degree * b.lat;
-  b_y = meters_per_degree * b.lng * Math.cos((b.lat * 2.0 * Math.PI) / 360.0);
-  return cluster.euclidean_distance([a_x, a_y], [b_x, b_y]);
+  return cluster.euclidean_distance(coord_func(a), coord_func(b));
 }
 
 var cluster_options = { 
   linkage_criteria: cluster.average_linkage(geo_distance),
   halting_condition: function(clusters, cluster_distance) { 
-    return cluster_distance >= 800000;
+    return cluster_distance >= 300000;  // Distance expressed in meters.
   },
   callback: function(clusters) {
     postMessage(clusters);
-    pausecomp(100);
+    //pausecomp(20);
   }
 };
 
